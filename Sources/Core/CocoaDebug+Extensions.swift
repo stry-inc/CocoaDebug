74a848ff41429e3cc306ec90b37df7d5fe178c00
Egor Petrov <egor@barbatosso.com>
2021-02-15 17:12:14 +0300

    Remove unneded code

--- Sources/Core/CocoaDebug+Extensions.swift
+++ Sources/Core/CocoaDebug+Extensions.swift
@@ -270,33 +270,6 @@ extension UIWindow {
     open override var canBecomeFirstResponder: Bool {
         return true
     }
-    
-    open override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        super.motionBegan(motion, with: event)
-        
-        self.cocoadebugShakeProperty = true
-        
-        if CocoaDebugSettings.shared.responseShake == false {return}
-        if motion == .motionShake {
-            if CocoaDebugSettings.shared.visible == true { return }
-            CocoaDebugSettings.shared.showBubbleAndWindow = !CocoaDebugSettings.shared.showBubbleAndWindow
-        }
-    }
-    
-    open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        super.motionEnded(motion, with: event)
-        
-        if self.cocoadebugShakeProperty == true {
-            self.cocoadebugShakeProperty = false
-            return
-        }
-        
-        if CocoaDebugSettings.shared.responseShake == false {return}
-        if motion == .motionShake {
-            if CocoaDebugSettings.shared.visible == true { return }
-            CocoaDebugSettings.shared.showBubbleAndWindow = !CocoaDebugSettings.shared.showBubbleAndWindow
-        }
-    }
 }
 
 
